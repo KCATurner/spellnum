997ab2910b61e2568772675a7a2c3d599f09b36e
Kevin Turner <kct0004@auburn.edu>
2018-07-17 12:49:26 -0500

    spellnum.py is now bilingual and spell_integer handles negative numbers

--- spellnum.py
+++ spellnum.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
 NUMB_LT_TWENTY = ('', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
                   "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen")
 TENS_GE_TWENTY = ("", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety")
@@ -10,9 +13,9 @@ ERROR_INVALID_PERIOD = 'period must be an integer in the range [0, 1000)'
 ERROR_INVALID_BASEILLION = 'baseillion must be an integer in the range [-1, 1000)'
 
 
-def _period_name(baseillion):
+def get_period_suffix(baseillion):
     """
-    Constructs and the period name from a three dimensional table of prefixes for each digit in the given base-illion
+    Constructs and the period name/suffix from a three dimensional table of prefixes for each digit in the given base-illion
     value; currently limited to base-illions less than 1000 (millinillion)
     
     :param baseillion: the base-illion value of the period
@@ -20,20 +23,33 @@ def _period_name(baseillion):
     """
     if not baseillion in range(-1, 1000, 1):
         raise ValueError(ERROR_INVALID_BASEILLION)
-    if baseillion < 0: return ''
-    if baseillion < 10: return UNIQUE_PERIODS[baseillion]
-    unit_prefix = PERIOD_PREFIXES_UNIT[(baseillion % 10) / 1]
-    tens_prefix = PERIOD_PREFIXES_TENS[(baseillion % 100) / 10]
-    hund_prefix = PERIOD_PREFIXES_HUND[(baseillion % 1000) / 100]
+    if baseillion < 0:
+        return ''
+    if baseillion < 10:
+        return UNIQUE_PERIODS[baseillion]
+
+    unit_prefix = PERIOD_PREFIXES_UNIT[(baseillion % 10) // 1]
+    tens_prefix = PERIOD_PREFIXES_TENS[(baseillion % 100) // 10]
+    hund_prefix = PERIOD_PREFIXES_HUND[(baseillion % 1000) // 100]
     result = "{0}{1}{2}llion".format(unit_prefix, tens_prefix, hund_prefix)
-    if not str(unit_prefix).endswith('e'): return result.replace("allion", "illion")
-    if 'sec' in result or 'seo' in result: unit_prefix += 'x'
-    if 'seq' in result or 'set' in result or 'sev' in result: unit_prefix += 's'
-    if 'trec' in result or 'treo' in result or 'treq' in result or 'tret' in result or 'trev' in result: unit_prefix += 's'
-    if 'septeo' in result or 'septev' in result: unit_prefix += 'm'
-    if 'septec' in result or 'septed' in result or 'septeq' in result or 'septes' in result or 'septet' in result: unit_prefix += 'n'
-    if 'noveo' in result or 'novev' in result: unit_prefix += 'm'
-    if 'novec' in result or 'noved' in result or 'noveq' in result or 'noves' in result or 'novet' in result: unit_prefix += 'n'
+
+    if not str(unit_prefix).endswith('e'):
+        return result.replace("allion", "illion")
+    if 'sec' in result or 'seo' in result:
+        unit_prefix += 'x'
+    if 'seq' in result or 'set' in result or 'sev' in result:
+        unit_prefix += 's'
+    if 'trec' in result or 'treo' in result or 'treq' in result or 'tret' in result or 'trev' in result:
+        unit_prefix += 's'
+    if 'septeo' in result or 'septev' in result:
+        unit_prefix += 'm'
+    if 'septec' in result or 'septed' in result or 'septeq' in result or 'septes' in result or 'septet' in result:
+        unit_prefix += 'n'
+    if 'noveo' in result or 'novev' in result:
+        unit_prefix += 'm'
+    if 'novec' in result or 'noved' in result or 'noveq' in result or 'noves' in result or 'novet' in result:
+        unit_prefix += 'n'
+
     return "{0}{1}{2}llion".format(unit_prefix, tens_prefix, hund_prefix).replace("allion", "illion")
 
 
@@ -46,12 +62,17 @@ def _spell_period(period):
     """
     if period not in range(0, 1000, 1):
         raise ValueError(ERROR_INVALID_PERIOD)
+
     unit = period % 10
     tens = period % 100 - unit
     hund = period - tens - unit
-    prefix = '{0} hundred'.format(NUMB_LT_TWENTY[hund/100]) if hund else ''
-    suffix = '{0}-{1}'.format(TENS_GE_TWENTY[tens/10], NUMB_LT_TWENTY[unit]).strip('-')
-    if tens < 20: suffix = NUMB_LT_TWENTY[tens + unit]
+
+    prefix = '{0} hundred'.format(NUMB_LT_TWENTY[hund//100]) if hund else ''
+    suffix = '{0}-{1}'.format(TENS_GE_TWENTY[tens//10], NUMB_LT_TWENTY[unit]).strip('-')
+
+    if tens < 20:
+        suffix = NUMB_LT_TWENTY[tens + unit]
+
     return '{0} {1}'.format(prefix, suffix).strip()
 
 
@@ -62,14 +83,25 @@ def spell_integer(number):
     :param number: a positive integer to be spelt
     :return: the spelling of the given integer as a string
     """
+    number = str(number)
+
+    if number == '-0':
+        return 'zero'
+    if number[0] == '-':
+        return 'negative {0}'.format(spell_integer(number=number[1:]))
+
     result = str()
     number = '{:,}'.format(int(number))
     periods = number.split(',')
     baseillion = len(periods) - 2
+
     for period in periods:
-        name = _period_name(baseillion=baseillion)
+        name = get_period_suffix(baseillion=baseillion)
         spelling = _spell_period(period=int(period))
+
         if int(period): # if the period is not '000'
             result += ' {0} {1}'.format(spelling, name)
+
         baseillion -= 1
+
     return result.strip() or 'zero'
