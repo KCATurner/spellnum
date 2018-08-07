8daa4ff6d62a70fb6a2516973f0d07638ec3ec59
Kevin Turner <kct0004@auburn.edu>
2018-08-07 10:15:01 -0500

    refactoring; lexicon now holds all word and prefix tuples

---
+++ spellnum/lexicon.py
@@ -0,0 +1,15 @@
+NUMS_LT_TWENTY = ('', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+                  'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen')
+
+TENS_GE_TWENTY = ('', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety')
+
+UNIQUE_PERIODS = ('thousand', 'million', 'billion', 'trillion', 'quadrillion',
+                  'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion')
+
+PERIOD_PREFIXES_UNIT = ('', 'un', 'duo', 'tre', 'quattuor', 'quinqua', 'se', 'septe', 'octo', 'nove')
+
+PERIOD_PREFIXES_TENS = ('', 'deci', 'viginti', 'triginta', 'quadraginta',
+                        'quinquaginta', 'sexaginta', 'septuaginta', 'octoginta', 'nonaginta')
+
+PERIOD_PREFIXES_HUND = ('', 'centi', 'ducenti', 'trecenti', 'quadringenti',
+                        'quingenti', 'sescenti', 'septingenti', 'octingenti', 'nongenti')
