c38afca3faff65708d793fe11f7caeaf366e7164
Kevin Turner <kct0004@auburn.edu>
2018-08-07 21:18:18 -0500

    added invalid spell_number input message for ValueErrors

--- spellnum/messages.py
+++ spellnum/messages.py
@@ -1,2 +1,3 @@
 ERROR_INVALID_PERIOD = 'period must be an integer in the range [0, 1000)'
 ERROR_INVALID_BASEILLION = 'baseillion must be an integer in the range [-1, 1000)'
+ERROR_INVALID_NUMBER = "we gave it our best, but we don't understand what you meant by {num}"
