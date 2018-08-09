a8e41c511db70fb3334467b07a7871f48290fd14
Kevin Turner <kct0004@auburn.edu>
2018-08-09 18:01:01 -0500

    added more comments and updated docstrings

--- spellnum/functions.py
+++ spellnum/functions.py
@@ -8,12 +8,13 @@ from spellnum import messages
 def get_period_suffix(base_illion):
     # type: (int) -> str
     """
-    Constructs and the period name/suffix from a three dimensional table of prefixes for each digit in the given
-    base-illion value; currently limited to base-illions less than 1000 (millinillion)
+    Constructs the period name/suffix from tuples of lexical components using each digit in the given base-illion
+    value; currently limited to base-illion values less than 1000 (millinillion)
     
-    :param base_illion: the base-illion suffix of the period name
-    :return: the name of the period with the given base-illion as a string
+    :param base_illion: The base-illion value of the period name
+    :return: The suffix for the period with the given base-illion
     """
+    # catches input outside function capabilities
     if base_illion not in range(-1, 1000, 1):
         raise ValueError(messages.ERROR_INVALID_BASEILLION)
     elif base_illion == -1:
@@ -21,12 +22,14 @@ def get_period_suffix(base_illion):
     elif base_illion < 10:
         return lexicon.UNIQUE_PERIODS[base_illion]
     
+    # builds suffix from lexical components
     result = '{unit}{tens}{hund}llion'.format(
-        unit=lexicon.PERIOD_PREFIXES_UNIT[(base_illion % 10) // 1],
-        tens=lexicon.PERIOD_PREFIXES_TENS[(base_illion % 100) // 10],
-        hund=lexicon.PERIOD_PREFIXES_HUND[(base_illion % 1000) // 100]
+        unit=lexicon.PERIOD_COMPONENTS_UNIT[(base_illion % 10) // 1],
+        tens=lexicon.PERIOD_COMPONENTS_TENS[(base_illion % 100) // 10],
+        hund=lexicon.PERIOD_COMPONENTS_HUND[(base_illion % 1000) // 100]
     )
     
+    # catches and corrects lexical component combination exceptions
     result = re.sub('(?<=^se)(?=[co])', repl='x', string=result)
     result = re.sub('(?<=^se)(?=[qtv])|(?<=^tre)(?=[coqtv])', repl='s', string=result)
     result = re.sub('(?<=^septe)(?=[ov])|(?<=^nove)(?=[ov])', repl='m', string=result)
@@ -43,16 +46,20 @@ def get_period_spelling(period):
     :param period: a positive integer less than 1000
     :return: the spelling of the given integer as a string
     """
+    # catches invalid input
     if period not in range(0, 1000, 1):
         raise ValueError(messages.ERROR_INVALID_PERIOD)
     
+    # splits period into components
     unit = period % 10
     tens = period % 100 - unit
     hund = period - tens - unit
     
-    prefix = '{0} hundred'.format(lexicon.NUMS_LT_TWENTY[hund//100]) if hund else ''
-    suffix = '{0}-{1}'.format(lexicon.TENS_GE_TWENTY[tens//10], lexicon.NUMS_LT_TWENTY[unit]).strip('-')
+    # composes prefix and suffix
+    prefix = '{h} hundred'.format(h=lexicon.NUMS_LT_TWENTY[hund//100]) if hund else ''
+    suffix = '{t}-{u}'.format(t=lexicon.TENS_GE_TWENTY[tens//10], u=lexicon.NUMS_LT_TWENTY[unit]).strip('-')
     
+    # overwrites suffix for special cases
     if tens < 20:
         suffix = lexicon.NUMS_LT_TWENTY[tens + unit]
         
@@ -66,6 +73,7 @@ def spell_number(num):
     :param num: a number to be spelt
     :return: the spelling of the given number as a string
     """
+    # parses input if given in scientific notation (preserves precession for string inputs)
     sci = re.match('^(?P<base>(?P<whole>\d+)\.?\d*)e\+?(?P<exp>-?\d+)$', string=str(num), flags=re.IGNORECASE)
     if sci:
         pos = sci.end('whole') + int(sci.group('exp'))
@@ -76,31 +84,38 @@ def spell_number(num):
             num = '0.{zeros}{num}'.format(zeros=''.zfill(abs(pos)), num=num)
         else:
             num = num[:pos] + '.' + num[pos:]
-    
+            
+    # catch invalid num input
     match = re.match('^-?0*(\d+)\.?(\d*[1-9]+)?0*$', string=str(num))
     if not match:
         raise ValueError(messages.ERROR_INVALID_NUMBER.format(num=num))
     
+    # split number into key components
     whole, fraction = match.group(1, 2)
     
+    # catches negative numbers
     if whole.startswith('-') and (int(whole) or int(fraction)):
         return 'negative {spelling}'.format(spelling=spell_number(num=num[1:]))
     
-    periods = '{:,}'.format(int(whole)).split(',')
+    # splits number into list of periods
+    periods = '{w:,}'.format(w=int(whole)).split(',')
+    # calculates base-illion value
     base_illion = len(periods) - 2
     
+    # constructs spelling for the whole number component
     result = str()
     for period in periods:
         if int(period):
-            result += ' {spelling} {name}'.format(
-                spelling=get_period_spelling(period=int(period)),
-                name=get_period_suffix(base_illion=base_illion)
+            result += ' {sp} {suf}'.format(
+                sp=get_period_spelling(period=int(period)),
+                suf=get_period_suffix(base_illion=base_illion)
             )
         base_illion -= 1
         
+    # appends spelling for any fraction component
     if fraction is not None:
         numerator = spell_number(fraction)
-        denominator = spell_number('{one}{z:0{width}d}'.format(one=1, z=0, width=len(fraction)))
-        result += '{a}{num} {den}ths'.format(a='and ' if result else '', num=numerator, den=denominator)
-    
+        denominator = spell_number('{one}{zeros}'.format(one=1, zeros=''.zfill(len(fraction))))
+        result += '{a}{num} {den}ths'.format(a=' and ' if result else '', num=numerator, den=denominator)
+        
     return result.strip() or 'zero'
