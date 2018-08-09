fbe8f99f473679658116279a223575330dbae5db
Kevin Turner <kct0004@auburn.edu>
2018-08-09 17:59:58 -0500

    refactored lexicon tuple names

--- spellnum/lexicon.py
+++ spellnum/lexicon.py
@@ -6,10 +6,10 @@ TENS_GE_TWENTY = ('', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'sevent
 UNIQUE_PERIODS = ('thousand', 'million', 'billion', 'trillion', 'quadrillion',
                   'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion')
 
-PERIOD_PREFIXES_UNIT = ('', 'un', 'duo', 'tre', 'quattuor', 'quinqua', 'se', 'septe', 'octo', 'nove')
+PERIOD_COMPONENTS_UNIT = ('', 'un', 'duo', 'tre', 'quattuor', 'quinqua', 'se', 'septe', 'octo', 'nove')
 
-PERIOD_PREFIXES_TENS = ('', 'deci', 'viginti', 'triginta', 'quadraginta',
-                        'quinquaginta', 'sexaginta', 'septuaginta', 'octoginta', 'nonaginta')
+PERIOD_COMPONENTS_TENS = ('', 'deci', 'viginti', 'triginta', 'quadraginta',
+                          'quinquaginta', 'sexaginta', 'septuaginta', 'octoginta', 'nonaginta')
 
-PERIOD_PREFIXES_HUND = ('', 'centi', 'ducenti', 'trecenti', 'quadringenti',
-                        'quingenti', 'sescenti', 'septingenti', 'octingenti', 'nongenti')
+PERIOD_COMPONENTS_HUND = ('', 'centi', 'ducenti', 'trecenti', 'quadringenti',
+                          'quingenti', 'sescenti', 'septingenti', 'octingenti', 'nongenti')
