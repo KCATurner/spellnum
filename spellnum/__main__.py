e6b4169208f5a7f619d09538cb21670b87966f9e
Kevin Turner <kevin.turner@nasa.gov>
2019-06-21 12:22:34 -0500

    Wasn't happy with the CLI output message format. Now it's colorful and I have yet another reason to hate Windows.

--- spellnum/__main__.py
+++ spellnum/__main__.py
@@ -6,8 +6,10 @@ Options:
   --help    Show this message and exit.
 """
 
+import sys
 import click
-import spellnum.functions
+from colorama import init, Fore
+from spellnum.functions import spell_number
 
 
 @click.command()
@@ -17,14 +19,22 @@ def cli(number, cosmic=False):
     """
     Command line interface for spellnum package.
     """
+    
+    # because Windows is just the wrong kind of special
+    if sys.platform == 'win32':
+        init()
+        
     spelling = str()
     while spelling != 'four':
         try:
-            spelling = spellnum.functions.spell_number(number)
-            print('\n{}\n{}'.format(number, spelling))
-            number = len(spelling.replace('-', '').replace(' ', ''))
+            spelling = spell_number(number)
+            if cosmic:
+                number = len(spelling.replace('-', '').replace(' ', ''))
+                print('There are ', Fore.CYAN, str(number), Fore.RESET,
+                      ' letters in ', Fore.LIGHTBLACK_EX, spelling, Fore.RESET, sep='')
+            else:
+                print(spelling)
+                break
         except ValueError as error:
             print(error)
-            break
-        if not cosmic:
             break
