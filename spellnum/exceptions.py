2849be6aa6da068c454c4a5c4a7ef21df78419c4
Kevin Turner <kevin.turner@nasa.gov>
2019-07-02 15:30:25 -0500

    No longer need BaseIllionOutOfBounds exception.

--- spellnum/exceptions.py
+++ spellnum/exceptions.py
@@ -3,18 +3,6 @@ Separate home for custom exceptions raised by spellnum functions.
 """
 
 
-class BaseIllionOutOfBounds(ValueError):
-    """
-    Custom exception type for get_period_suffix function
-    """
-    
-    def __init__(self, value, *args):
-        super(BaseIllionOutOfBounds, self).__init__(
-            "{} isn't an integer in the range [-1, 1000)!".format(value),
-            *args
-        )
-        
-        
 class InvalidNumericalFormat(ValueError):
     """
     Custom exception type for spell_number function
