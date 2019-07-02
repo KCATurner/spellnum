f5ede053a4e687d070a52aee5e3233e44dca375a
Kevin Turner <kevin.turner@nasa.gov>
2019-07-02 15:49:04 -0500

    Renamed regex for numeric strings.

--- spellnum/regexlib.py
+++ spellnum/regexlib.py
@@ -10,16 +10,15 @@ S_LEXICAL_EXCEPTION = _re.compile(r'(?<=^se)(?=[qtv])|(?<=^tre)(?=[coqtv])')
 M_LEXICAL_EXCEPTION = _re.compile(r'(?<=^septe)(?=[ov])|(?<=^nove)(?=[ov])')
 N_LEXICAL_EXCEPTION = _re.compile(r'(?<=^septe)(?=[cdqst])|(?<=^nove)(?=[cdqst])')
 
-VALID_NUMERIC_FLOAT = _re.compile((r'^(?P<sign>[-+]?)(?# capture sign if exists)'
-                                   r'0*(?# match, but exclude leading zeros from whole)'
-                                   r'(?P<whole>\d+)?(?# capture whole number value)'
-                                   r'\.?(?# match decimal that may come after whole number)'
-                                   r'(?P<fraction>(?<=\.)(?# must follow decimal)\d*[1-9])?'
-                                   r'0*(?# match, but exclude trailing zeros from fraction)'
-                                   r'(?<!\.)(?# e/E cannot follow decimal without fraction)'
-                                   r'[eE]?(?# match, but exclude e/E from exponent)'
-                                   r'(?P<exponent>(?<=[eE])(?# must follow e/E)[-+]?\d+)'
-                                   r'?(?<=\d)(?# match must end with at least one digit)$'))
+VALID_NUMERIC_STRING = _re.compile((r'^(?P<sign>[-+])?(?# capture sign if exists)'
+                                    r'0*(?# match, but exclude leading zeros from whole)'
+                                    r'(?P<whole>\d+)?(?# capture whole number value)'
+                                    r'(?:\.(?# only match fraction following decimal)'
+                                    r'(?P<fraction>\d*[1-9])?(?# capture fraction value)'
+                                    r'0*)?(?# match, but exclude trailing zeros from fraction)'
+                                    r'(?:(?<!\.)[eE](?# only match exponent following e/E)'
+                                    r'(?P<exponent>[-+]?\d+))?(?# capture exponent value)'
+                                    r'(?<=\d)$(?# number must end in a digit to be valid)'))
 
 FRACTION_SPELLING_FORMAT = _re.compile(r'^(?:(?P<whole>.+)\s+and\s+)?(?# " and " separates whole from fraction)'
                                        r'(?P<fnumer>.+\s+)?(?# fraction made of numerator and base-10 denominator)'
