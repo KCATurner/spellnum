d80dee86cefe1852b00bd663d824a94c629dd88b
Kevin Turner <kevin.turner@nasa.gov>
2019-07-11 13:00:09 -0500

    Reworked text patterns

--- spellnum/regexlib.py
+++ spellnum/regexlib.py
@@ -2,26 +2,35 @@
 Separate home for useful, precompiled regular expressions.
 """
 
-import re as _re
+import re as __re
 
 
-X_LEXICAL_EXCEPTION = _re.compile(r'(?<=^se)(?=[co])')
-S_LEXICAL_EXCEPTION = _re.compile(r'(?<=^se)(?=[qtv])|(?<=^tre)(?=[coqtv])')
-M_LEXICAL_EXCEPTION = _re.compile(r'(?<=^septe)(?=[ov])|(?<=^nove)(?=[ov])')
-N_LEXICAL_EXCEPTION = _re.compile(r'(?<=^septe)(?=[cdqst])|(?<=^nove)(?=[cdqst])')
+NUMERIC_STRING_PATTERN = __re.compile((r'^(?P<sign>[-+])?(?# capture sign if exists)'
+                                       r'0*(?# match, but exclude leading zeros from whole)'
+                                       r'(?P<whole>\d+)?(?# capture whole number value)'
+                                       r'(?:\.(?# only match fraction following decimal)'
+                                       r'(?P<fraction>\d*[1-9])?(?# capture fraction value)'
+                                       r'0*)?(?# match, but exclude trailing zeros from fraction)'
+                                       r'(?:(?<!\.)[eE](?# only match exponent following e/E)'
+                                       r'(?P<exponent>[-+]?\d+))?(?# capture exponent value)'
+                                       r'(?<=\d)$(?# number must end in a digit to be valid)'))
 
-VALID_INPUT_NUMBER = _re.compile((r'^(?P<sign>[-+])?(?# capture sign if exists)'
-                                  r'0*(?# match, but exclude leading zeros from whole)'
-                                  r'(?P<whole>\d+)?(?# capture whole number value)'
-                                  r'(?:\.(?# only match fraction following decimal)'
-                                  r'(?P<fraction>\d*[1-9])?(?# capture fraction value)'
-                                  r'0*)?(?# match, but exclude trailing zeros from fraction)'
-                                  r'(?:(?<!\.)[eE](?# only match exponent following e/E)'
-                                  r'(?P<exponent>[-+]?\d+))?(?# capture exponent value)'
-                                  r'(?<=\d)$(?# number must end in a digit to be valid)'))
+FRACTION_TEXT_PATTERN = __re.compile((r'^(?P<whole>.+\w)?(?# capture whole number text)'
+                                      r'(?<!th)(?<!ths)(?# whole portion cannot end in th/ths)'
+                                      r'(?(whole)(?# if any whole portion is captured...)'
+                                      r'(?=\s+and\s+(?# ... anticipate " and " separator ...)'
+                                      r'|$)(?# ... or end of string...)|(?# ...otherwise nothing))'
+                                      r'(?:\s+and\s+)?(?# match " and " if possible without capturing)'
+                                      r'(?:(?P<numerator>.+\w)(?# capture fraction numerator text)'
+                                      r'\s+(?# whitespace must separate numerator and denominator)'
+                                      r'(?P<denominator>(?# capture fraction denominator text)'
+                                      r'(?:\bone\s+hundred|\bten|\bone\b)(?# denominator period value)'
+                                      r'\s*\w*)ths?(?# to capture fraction, must end in th/ths)'
+                                      r')?$(?# string must end after whole or fraction)'))
 
-VALID_INPUT_TEXT = _re.compile((r'^(?:(?P<whole>.+)\s+and\s+)?(?# " and " separates whole from fraction)'
-                                r'(?P<numerator>.+\s+)?(?# fraction made of numerator and base-10 denominator)'
-                                r'(?P<denominator>(?:one hundred|ten|one\s+)(?# denominator period value)'
-                                r'\s*\w*)(?# capture period suffix if exists)'
-                                r'ths?$(?# spelling is only valid if it ends in "th" or "ths")'))
+# TODO: comment this expression
+PERIOD_TEXT_PATTERN = __re.compile((r'(?:^|\s+)(?# )'
+                                    r'\b(?P<value>.+?)(?# )'
+                                    r'(?:\s+(?# )'
+                                    r'(?P<period>\w+illion\b|thousand\b)(?# )'
+                                    r'|\s*$)(?# )'))
