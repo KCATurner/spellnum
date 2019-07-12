c585d801bb3dcac1302f9b7c6e020245c3aa23e8
Kevin Turner <kevin.turner@nasa.gov>
2019-07-11 20:06:43 -0500

    Updated comments.

--- spellnum/lexicon.py
+++ spellnum/lexicon.py
@@ -1,11 +1,23 @@
 """
 Separate home for lexical components used by spellnum functions.
+
+Attributes:
+    INTEGERS_LT_100 (tuple): index-aligned 2 digit spellings
+    INTEGERS_LT_1000 (tuple): index-aligned 3 digit spellings
+    COMPOSITE_PERIOD_PREFIXES (tuple): index-aligned prefixes for any base-illion period value
+    
+    _UNIQUE_WORDS (tuple): index-aligned unique spellings
+    _UNIQUE_PERIOD_NAMES (tuple): index-aligned unique period names (base-illion 0-9)
+    _PREFIX_COMPONENTS_UNIT (tuple): index-aligned period prefix components for base-illion units
+    _PREFIX_COMPONENTS_TENS (tuple): index-aligned period prefix components for base-illion tens
+    _PREFIX_COMPONENTS_HUND (tuple): index-aligned period prefix components for base-illion hundreds
+    
 """
+
 
 import re as __re
 
 
-# index-aligned unique spellings
 _UNIQUE_WORDS = ('', 'one', 'two', 'three', 'four',
                  'five', 'six', 'seven', 'eight', 'nine',
                  'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
@@ -15,34 +27,28 @@ _UNIQUE_WORDS = ('', 'one', 'two', 'three', 'four',
                  + ('sixty',) + ('',)*9 + ('seventy',) + ('',)*9\
                  + ('eighty',) + ('',)*9 + ('ninety',) + ('',)*9
 
-# index-aligned 2 digit spellings
 INTEGERS_LT_100 = _UNIQUE_WORDS[:20]\
                   + tuple('{}-{}'.format(_UNIQUE_WORDS[10*(i//10)],
                                          _UNIQUE_WORDS[i % 10]
                                          ).strip('-') for i in range(20, 100))
 
-# index-aligned 3 digit spellings
 INTEGERS_LT_1000 = tuple('{} {}'.format(INTEGERS_LT_100[i//100]
                                         + ' hundred' if i >= 100 else '',
                                         INTEGERS_LT_100[i % 100]
                                         ).strip() for i in range(1000))
 
 
-# index-aligned unique period names (base-illion 0-9)
 _UNIQUE_PERIOD_NAMES = ('nillion', 'million', 'billion', 'trillion',
                         'quadrillion', 'quintillion', 'sextillion',
                         'septillion', 'octillion', 'nonillion')
 
-# index-aligned period prefix components for base-illion units
 _PREFIX_COMPONENTS_UNIT = ('', 'un', 'duo', 'tre', 'quattuor',
                            'quinqua', 'se', 'septe', 'octo', 'nove')
 
-# index-aligned period prefix components for base-illion tens
 _PREFIX_COMPONENTS_TENS = ('', 'deci', 'viginti', 'triginta',
                            'quadraginta', 'quinquaginta', 'sexaginta',
                            'septuaginta', 'octoginta', 'nonaginta')
 
-# index-aligned period prefix components for base-illion hundreds
 _PREFIX_COMPONENTS_HUND = ('', 'centi', 'ducenti', 'trecenti',
                            'quadringenti', 'quingenti', 'sescenti',
                            'septingenti', 'octingenti', 'nongenti')
@@ -71,9 +77,9 @@ def __buildprefix(base_illion):
         prefix = __M_LEXICAL_EXCEPTION.sub(repl='m', string=prefix)
         prefix = __N_LEXICAL_EXCEPTION.sub(repl='n', string=prefix)
         
+    # prefix shouldn't end in "a" or "i"
     return prefix.rstrip('ai')
 
 
-# index-aligned prefixes for any base-illion period value
 COMPOSITE_PERIOD_PREFIXES = tuple(__s.replace('illion', '') for __s in _UNIQUE_PERIOD_NAMES) \
                             + tuple(__buildprefix(__i) for __i in range(10, 1000))
