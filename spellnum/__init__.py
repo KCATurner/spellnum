c339e53bc0e6e6e3a22b7e2846b79e42cb1e4d34
Kevin Turner <kevin.turner@nasa.gov>
2019-10-31 13:18:54 -0500

    Moved natural number tuples to top of package.

--- spellnum/__init__.py
+++ spellnum/__init__.py
@@ -1,3 +1,9 @@
-from spellnum.functions import *
+"""
+TODO...
+"""
 
-__name__ = "spellnum"
+# bring core elements to top of package
+from spellnum.lexicon import NATURAL_NUMBERS_LT_100
+from spellnum.lexicon import NATURAL_NUMBERS_LT_1000
+from spellnum.functions import nameperiod, readperiod
+from spellnum.functions import number2text, text2number
