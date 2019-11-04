4e2b7172c5755ca82480f343ab6811dcb0158b87
Kevin Turner <kevin.turner@nasa.gov>
2019-11-04 12:56:14 -0600

    Refactored CLI and replaced four command with -r/--recursive option for spell.

--- spellnum/__main__.py
+++ spellnum/__main__.py
@@ -1,65 +1,55 @@
 """
-usage: spellnum [-h] {read,spell,four} ...
-
-positional arguments:
-  {read,spell,four}
-    read             convert text to number
-    spell            convert number to text
-    four             spell recursively to four
-
-optional arguments:
-  -h, --help         show this help message and exit
+Home to the entry point for spellnum's command line interface.
 """
 
 from __future__ import print_function
 
 import sys
 import pyperclip
-from colorama import init, Fore
-from argparse import ArgumentParser
-from spellnum.lexicon import NATURAL_NUMBERS_LT_100
-from spellnum.functions import number2text, text2number
+import colorama
+import argparse
+import spellnum
 
 
-def __read(number, copy=False):
+def __read(text, copy=False):
     """ For internal use only! """
-    result = text2number(text=number)
+    result = spellnum.text2number(text=text)
     
     if copy:
         pyperclip.copy(result)
-    
+        
     print(result)
-
-
+    
+    
 def __spell(number, copy=False):
     """ For internal use only! """
-    result = number2text(number=number)
+    result = spellnum.number2text(number=number)
     
     if copy:
         pyperclip.copy(result)
-    
+        
     message = list()
     for word in result.split(' '):
-        if word in ('hundred',) + NATURAL_NUMBERS_LT_100:
-            message.append(Fore.LIGHTBLACK_EX + word)
+        if word in ('hundred',) + spellnum.NATURAL_NUMBERS_LT_100:
+            message.append(colorama.Fore.LIGHTBLACK_EX + word)
         elif word in ('negative', 'and'):
-            message.append(Fore.YELLOW + word)
+            message.append(colorama.Fore.YELLOW + word)
         else: # (period names)
-            message.append(Fore.RESET + word)
+            message.append(colorama.Fore.RESET + word)
             
     message = ' '.join(message)
     print(message)
-
-
+    
+    
 def __four(number, copy=False):
     """ For internal use only! """
     result = spelling = str()
     while spelling != 'four':
         try:
-            spelling = number2text(number)
+            spelling = spellnum.number2text(number)
             number = len(spelling.replace('-', '').replace(' ', ''))
-            print('There are ', Fore.CYAN, str(number), Fore.RESET,
-                  ' letters in ', Fore.LIGHTBLACK_EX, spelling, Fore.RESET, sep='')
+            print('There are ', colorama.Fore.CYAN, str(number), colorama.Fore.RESET,
+                  ' letters in ', colorama.Fore.LIGHTBLACK_EX, spelling, colorama.Fore.RESET, sep='')
         except ValueError as error:
             print(error)
             break
@@ -70,34 +60,45 @@ def __four(number, copy=False):
         
 def cli():
     """
-    Command line interface for spellnum package.
+    Entry point for spellnum's command line interface.
     """
     
     # because Windows is just the wrong kind of special
     if sys.platform == 'win32':
-        init()
+        colorama.init()
         
-    arguments = ArgumentParser(add_help=False)
-    arguments.add_argument('number', type=str, help='number or text to convert')
-    arguments.add_argument('-c', '--copy', action='store_true', help='copy result to clipboard')
+    # reusable copy option
+    copy_option = argparse.ArgumentParser(add_help=False)
+    copy_option.add_argument(
+        '-c', '--copy', action='store_true',
+        help='copy output to clipboard')
     
-    parser = ArgumentParser()
-    command = parser.add_subparsers(dest='command', required=True)
+    parser = argparse.ArgumentParser()
+    command = parser.add_subparsers(metavar='SUBCOMMAND')
     
-    read = command.add_parser('read', parents=[arguments], help='convert text to number')
+    read = command.add_parser(
+        'read', parents=[copy_option],
+        help='convert text to number')
+    read.add_argument(
+        'text', type=str,
+        help='text of a number to read')
     read.set_defaults(func=__read)
     
-    spell = command.add_parser('spell', parents=[arguments], help='convert number to text')
+    spell = command.add_parser(
+        'spell', parents=[copy_option],
+        help='convert number to text')
+    spell.add_argument(
+        'number', type=str,
+        help='a number to be spelled')
+    spell.add_argument(
+        '-r', '--recursive', action='store_const', const=__four, dest='func',
+        help="spell recursively to 'four'")
     spell.set_defaults(func=__spell)
     
-    four = command.add_parser('four', parents=[arguments], help='spell recursively to four')
-    four.set_defaults(func=__four)
-    
+    # call func with parsed args
     inputs = vars(parser.parse_args())
-    func = inputs.pop('func')
-    inputs.pop('command')
-    func(**inputs)
-
-
+    inputs.pop('func', parser.print_help)(**inputs)
+    
+    
 if __name__ == '__main__':
     cli()
