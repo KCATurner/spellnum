6114b5c6d600388fddf5330ad6e2420a694b34ce
Kevin Turner <kevin.turner@nasa.gov>
2019-11-06 14:29:35 -0600

    Refactored CLI entry point.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     ],
     entry_points={
         'console_scripts': [
-            'conwech = conwech.__main__:cli'
+            'conwech = conwech.__main__:main'
         ]
     },
     classifiers=[
