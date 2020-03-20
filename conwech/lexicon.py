319638dbbaab927221bbd1a96d87f5c71624b60a
Kevin Turner <kct0004@auburn.edu>
2020-03-20 15:27:58 -0500

    Fixed intentional test error.

--- conwech/lexicon.py
+++ conwech/lexicon.py
@@ -108,7 +108,7 @@ Prefix components for the tens digit of a zillion period.
 
 
 _HUND_PREFIX_COMPONENTS = (
-    '', 'centi', 'duocenti', 'trecenti', 'quadringenti', 'quingenti',
+    '', 'centi', 'ducenti', 'trecenti', 'quadringenti', 'quingenti',
     'sescenti', 'septingenti', 'octingenti', 'nongenti')
 """
 Prefix components for the hundreds digit of a zillion period.
