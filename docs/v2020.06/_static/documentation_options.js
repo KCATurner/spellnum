d483f6f96e69138c0efbcfcd7e9bfadc4691997f
CircleCI <kct0004@auburn.edu>
2020-06-12 22:47:06 +0000

    [skip ci] documentation build.

--- docs/v2020.06/_static/documentation_options.js
+++ docs/v2020.06/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2020.06rc1',
+    VERSION: 'v2020.06rc2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
