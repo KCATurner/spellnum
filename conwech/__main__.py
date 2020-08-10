72ce9dee518cd037032bc17ae967641476774d51
Kevin Turner <kct0004@auburn.edu>
2020-08-10 13:27:06 -0500

    Refactored for easier unit testing.

--- conwech/__main__.py
+++ conwech/__main__.py
@@ -6,13 +6,44 @@ Home to the entry point for ConWech's command line interface.
 from __future__ import print_function
 
 # built-in modules
+import re
 import sys
+import enum
 import pyperclip
 import colorama
 import argparse
 
 # project modules
 import conwech
+
+
+# because Windows is just the wrong kind of special
+if sys.platform == 'win32':
+    colorama.init()
+
+
+class Colors(enum.Enum):
+    numeral = colorama.Fore.LIGHTBLACK_EX
+    numeric = colorama.Fore.CYAN
+    other = colorama.Fore.YELLOW
+    reset = colorama.Style.RESET_ALL
+
+
+def paint(numeral):
+    """ For internal use only! """
+    result = []
+    for word in numeral.split(' '):
+        if word in ('hundred', ) + conwech.NATURAL_NUMBERS_LT_100:
+            result.append(Colors.numeral.value + word)
+        elif word in ('negative', 'and'):
+            result.append(Colors.other.value + word)
+        elif re.match(r'(.+illion|thousand)(ths?)?', word):
+            result.append(Colors.other.value + word)
+        elif word.isdigit():
+            result.append(Colors.numeric.value + word)
+        else:
+            result.append(Colors.reset.value + word)
+    return ' '.join(result)
 
 
 def read(text, copy=False):
@@ -32,74 +63,56 @@ def spell(number, copy=False):
     if copy:
         pyperclip.copy(result)
 
-    message = list()
-    for word in result.split(' '):
-        if word in ('hundred',) + conwech.NATURAL_NUMBERS_LT_100:
-            message.append(colorama.Fore.LIGHTBLACK_EX + word)
-        elif word in ('negative', 'and'):
-            message.append(colorama.Fore.YELLOW + word)
-        else: # (period names)
-            message.append(colorama.Fore.RESET + word)
-
-    message = ' '.join(message)
-    print(message)
+    print(paint(result))
 
 
 def four(number, copy=False):
     """ For internal use only! """
-    result = spelling = str()
+    result = []
+    spelling = ''
 
     while spelling != 'four':
-        try:
-            spelling = conwech.number2text(number)
-            number = len(spelling.replace('-', '').replace(' ', ''))
-            print('There are ', colorama.Fore.CYAN, str(number), colorama.Fore.RESET,
-                  ' letters in ', colorama.Fore.LIGHTBLACK_EX, spelling, colorama.Fore.RESET, sep='')
-        except ValueError as error:
-            print(error)
-            break
+        spelling = conwech.number2text(number)
+        number = len(spelling.replace('-', '').replace(' ', ''))
+        result.append('There are {} letters in {}'.format(number, spelling))
+        print(paint(result[-1]))
 
     if copy:
-        pyperclip.copy(result)
-
-
-# because Windows is just the wrong kind of special
-if sys.platform == 'win32':
-    colorama.init()
-
-# reusable copy option
-copy_option = argparse.ArgumentParser(add_help=False)
-copy_option.add_argument(
-    '-c', '--copy', action='store_true',
-    help='copy output to clipboard')
-
-parser = argparse.ArgumentParser()
-commands = parser.add_subparsers(metavar='SUBCOMMAND')
-
-read_command = commands.add_parser(
-    'read', parents=[copy_option],
-    help='convert text to number')
-read_command.add_argument(
-    'text', type=str,
-    help='text of a number to read')
-read_command.set_defaults(func=read)
-
-spell_command = commands.add_parser(
-    'spell', parents=[copy_option],
-    help='convert number to text')
-spell_command.add_argument(
-    'number', type=str,
-    help='a number to be spelled')
-spell_command.add_argument(
-    '-r', '--recursive', action='store_const', const=four, dest='func',
-    help="spell recursively to 'four'")
-spell_command.set_defaults(func=spell)
+        pyperclip.copy('\n'.join(result))
 
 
 def main():
+    """ For internal use only! """
+    copy_option = argparse.ArgumentParser(add_help=False)
+    copy_option.add_argument(
+        '-c', '--copy', action='store_true',
+        help='copy output to clipboard')
+
+    parser = argparse.ArgumentParser()
+    commands = parser.add_subparsers(metavar='SUBCOMMAND')
+
+    read_command = commands.add_parser(
+        'read', parents=[copy_option],
+        help='convert text to number')
+    read_command.add_argument(
+        'text', type=str,
+        help='text of a number to read')
+    read_command.set_defaults(func=read)
+
+    spell_command = commands.add_parser(
+        'spell', parents=[copy_option],
+        help='convert number to text')
+    spell_command.add_argument(
+        'number', type=str,
+        help='a number to be spelled')
+    spell_command.add_argument(
+        '-r', '--recursive', action='store_const', const=four, dest='func',
+        help="spell recursively to 'four'")
+    spell_command.set_defaults(func=spell)
+
     # call func with parsed args
     inputs = vars(parser.parse_args())
-    inputs.pop('func', parser.print_help)(**inputs)
+    inputs.pop('func', parser.print_help)(**inputs) # noqa
 
 
 if __name__ == '__main__':
