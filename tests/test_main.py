61c8a6e147b0cab4cf88623f7ddd4648c41aad0c
Kevin Turner <kct0004@auburn.edu>
2020-08-10 13:28:08 -0500

    Added basic unit tests for coverage of __main__.

---
+++ tests/test_main.py
@@ -0,0 +1,56 @@
+"""
+Unit tests for the conwech.__main__.
+"""
+
+import re
+import pytest
+import pyperclip
+import subprocess
+import conwech.__main__
+
+
+class TestRead:
+
+    @pytest.mark.parametrize(
+        argnames='numeral',
+        argvalues=['negative twelve thousand and three tenths', ])
+    def test_copy_flag(self, numeral):
+        pyperclip.copy('test')
+        output = subprocess.run(
+            f'conwech read "{numeral}"',
+            capture_output=True, text=True, check=True).stdout
+        assert pyperclip.paste() == 'test' # clipboard should be unaffected
+        output = subprocess.run(
+            f'conwech read --copy "{numeral}"',
+            capture_output=True, text=True, check=True).stdout
+        for color in conwech.__main__.Colors:
+            output = output.replace(color.value, '')
+        assert pyperclip.paste() == output.strip() # clipboard should hold output
+
+
+class TestSpell:
+
+    @pytest.mark.parametrize(
+        argnames='number',
+        argvalues=['-12000.3'])
+    def test_copy_flag(self, number):
+        pyperclip.copy('test')
+        output = subprocess.run(
+            f'conwech spell "{number}"',
+            capture_output=True, text=True, check=True).stdout
+        assert pyperclip.paste() == 'test' # clipboard should be unaffected
+        output = subprocess.run(
+            f'conwech spell --copy "{number}"',
+            capture_output=True, text=True, check=True).stdout
+        for color in conwech.__main__.Colors:
+            output = output.replace(color.value, '')
+        assert pyperclip.paste() == output.strip() # clipboard should hold output
+
+    def test_recursive_flag(self):
+        output = subprocess.run(
+            f'conwech spell --recursive "323"',
+            capture_output=True, text=True, check=True).stdout
+        for color in conwech.__main__.Colors:
+            output = output.replace(color.value, '')
+        numbers = [int(n) for n in re.findall(r'\b(\d+)\b', output)]
+        assert numbers == [23, 11, 6, 3, 5, 4, 4]
